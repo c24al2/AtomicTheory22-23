06ba0ad5fcadcd951d2791004f017854fd6116b7
Max Larsson <maxslarsson@gmail.com>
2022-11-18 02:41:31 -0500

    Rename teleop to opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IterativeOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IterativeOpmode.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.teleop;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
