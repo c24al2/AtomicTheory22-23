da3aba45b49386b2793e31ff1145d46050e9fc38
Max Larsson <maxslarsson@gmail.com>
2023-01-22 19:27:35 -0500

    Add Drive teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Drive.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleOmniDrive;
+
+@TeleOp
+public class Drive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleOmniDrive drive = new SampleOmniDrive(hardwareMap);
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            drive.update();
+        }
+    }
+}
