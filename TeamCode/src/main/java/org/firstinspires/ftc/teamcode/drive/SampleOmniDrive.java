bf5e806eb8f6d7bcd564061164780234d43b6971
Max Larsson <maxslarsson@gmail.com>
2023-01-22 19:29:21 -0500

    Make distance to each wheel more accurate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleOmniDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleOmniDrive.java
@@ -48,9 +48,9 @@ import java.util.List;
 @Config
 public class SampleOmniDrive extends OmniDrive {
     private static final List<Pose2d> WHEEL_POSES = Arrays.asList(
-            new Pose2d(7.5*Math.sin(Math.toRadians(30)), 7.5*Math.cos(Math.toRadians(30)), Math.toRadians(150)),  // Front left
-            new Pose2d(-7.5, 0, Math.toRadians(270)), // Back wheel
-            new Pose2d(7.5*Math.sin(Math.toRadians(30)), -7.5*Math.cos(Math.toRadians(30)), Math.toRadians(30)) // Front right
+            new Pose2d(7.2*Math.sin(Math.toRadians(30)), 7.2*Math.cos(Math.toRadians(30)), Math.toRadians(150)),  // Front left
+            new Pose2d(-7.2, 0, Math.toRadians(270)), // Back wheel
+            new Pose2d(7.2*Math.sin(Math.toRadians(30)), -7.2*Math.cos(Math.toRadians(30)), Math.toRadians(30)) // Front right
     );
 
     private static final boolean USE_EXTERNAL_HEADING = true;
