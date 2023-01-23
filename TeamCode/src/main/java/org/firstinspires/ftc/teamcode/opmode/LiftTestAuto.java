67d17a7550823083593839f26b16ca4256586213
c24al2 <c24al2@dalton.org>
2023-01-23 03:31:39 -0500

    Implement Autos to test lift encoder values with

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/LiftTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/LiftTestAuto.java
@@ -1,5 +1,36 @@
 package org.firstinspires.ftc.teamcode.opmode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class LiftTestAuto {
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.IntakeandLiftPID;
+import org.firstinspires.ftc.teamcode.drive.PoseStorage;
+import org.firstinspires.ftc.teamcode.drive.SampleOmniDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.vision.CameraController;
+import org.firstinspires.ftc.teamcode.vision.ParkingPositionPipeline;
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+
+@Autonomous
+public class LiftTestAuto extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleOmniDrive drive = new SampleOmniDrive(hardwareMap);
+        IntakeandLiftPID liftandServo = new IntakeandLiftPID(hardwareMap);
+
+        liftandServo.intakeFullStep(DriveConstants.GROUNDJUNCTION);
+        liftandServo.intakeFullStep(DriveConstants.LOWJUNCTION);
+        liftandServo.intakeFullStep(DriveConstants.MEDIUMJUNCTION);
+        liftandServo.intakeFullStep(DriveConstants.HIGHJUNCTION);
+        sleep(4000);
+    }
+
+
 
 }
