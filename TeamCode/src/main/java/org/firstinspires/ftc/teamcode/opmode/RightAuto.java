9ed99db18863da2808993deb2f358a1cae91673d
Max Larsson <maxslarsson@gmail.com>
2023-01-22 19:27:30 -0500

    Rename 'autos' folder to 'opmode'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/RightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/RightAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.autos;
+package org.firstinspires.ftc.teamcode.opmode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
