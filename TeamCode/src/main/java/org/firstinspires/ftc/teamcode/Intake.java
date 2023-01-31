a18a3aa2a8418dce99880b33bb9e1498c8dbf531
maxslarsson <maxslarsson@gmail.com>
2023-01-30 21:30:19 -0500

    Rewrote the intake code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.control.PIDFController;
+import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionState;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
+
+@Config
+public class Intake {
+    public static PIDCoefficients INTAKE_PID = new PIDCoefficients(.009, 0, 0.0002);
+    public static double kV = 0;
+    public static double kA = 0;
+    public static double kStatic = 0;
+
+    public static double GRAVITY_ACCEL = 200; // Constant feedforward acceleration (in ticks/sec/sec) to counteract the lift
+    public static double MAX_VEL = 62000;
+    public static double MAX_ACCEL = 2000;
+    public static double MAX_JERK = 0;  // Jerk isn't used if it's 0, but it might end up being necessary
+
+    public ElapsedTime timer;
+
+    public DcMotorEx intake;
+    public Servo clawServo;
+
+    private PIDFController controller;
+    private MotionProfile motionProfile;
+
+    public Intake(HardwareMap hardwareMap) {
+        LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap);
+
+        timer = new ElapsedTime();
+
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
+
+        intake.setDirection(DcMotor.Direction.REVERSE);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Turns on bulk reading
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        controller = new PIDFController(INTAKE_PID, kV, kA, kStatic);
+    }
+
+    public void openClaw() {
+        clawServo.setPosition(1.0);
+    }
+    public void closeClaw(){
+        clawServo.setPosition(0.8);
+    }
+
+    public double getTargetPosition() {
+        return motionProfile.end().getX();
+    }
+
+    public void setTargetPosition(double targetPosition) {
+        motionProfile = MotionProfileGenerator.generateSimpleMotionProfile(
+                new MotionState(intake.getCurrentPosition(), intake.getVelocity(), 0),
+                new MotionState(targetPosition, 0, 0),
+                MAX_VEL,
+                MAX_ACCEL,
+                MAX_JERK
+        );
+
+        timer.reset();
+    }
+
+    public void setRelativeTargetPosition(double deltaTargetPosition) {
+        double newTargetPosition = getTargetPosition() + deltaTargetPosition;
+        setTargetPosition(newTargetPosition);
+    }
+
+    public void followMotionProfile() {
+        MotionState state = motionProfile.get(timer.time());
+
+        controller.setTargetPosition(state.getX());
+        controller.setTargetVelocity(state.getV());
+        controller.setTargetAcceleration(state.getA() + GRAVITY_ACCEL);
+
+        double power = controller.update(intake.getCurrentPosition(), intake.getVelocity());
+        intake.setPower(power);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Drive.java
@@ -3,13 +3,12 @@ package org.firstinspires.ftc.teamcode.opmode;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.drive.IntakeandLiftPID;
-import org.firstinspires.ftc.teamcode.drive.PoseStorage;
+import org.firstinspires.ftc.teamcode.Intake;
 import org.firstinspires.ftc.teamcode.drive.SampleOmniDrive;
 
 
@@ -17,22 +16,27 @@ import org.firstinspires.ftc.teamcode.drive.SampleOmniDrive;
 @TeleOp
 public class Drive extends OpMode {
     private static double SLOW_MODE_SCALAR = 0.4;
+    private static double LIFT_MULTIPLIER = 800; // TICKS/SEC
 
     // TODO: Remove, this is for debugging purposes
 //        private static Pose2d START_POSE = PoseStorage.currentPose;
     private static Pose2d START_POSE = new Pose2d(36, -62.8, Math.toRadians(90));
+
+    private ElapsedTime timer;
 
     private boolean driverSlowMode = false;
     private Gamepad previousGamepad1 = new Gamepad();
     private Gamepad previousGamepad2 = new Gamepad();
 
     public SampleOmniDrive drive;
-    public IntakeandLiftPID liftandServo;
+    public Intake intake;
 
     @Override
     public void init() {
+        timer = new ElapsedTime();
+
         drive = new SampleOmniDrive(hardwareMap);
-        liftandServo = new IntakeandLiftPID(hardwareMap);
+        intake = new Intake(hardwareMap);
 
         drive.setPoseEstimate(START_POSE);
     }
@@ -62,40 +66,35 @@ public class Drive extends OpMode {
         // Rotation is not part of the rotated input thus must be passed in separately
         drive.setWeightedDrivePower(input);
 
-//        if (Math.abs(gamepad2.left_stick_y) > 0.2) {
-//            liftandServo.setIntakePower(-gamepad2.left_stick_y);
-//        }
+        intake.setRelativeTargetPosition(-gamepad2.left_stick_y * LIFT_MULTIPLIER * timer.time());
+        timer.reset();
 
         if (gamepad2.x){
-            liftandServo.clawClose();
+            intake.closeClaw();
         }
+
         if (gamepad2.y){
-            liftandServo.clawOpen();
+            intake.openClaw();
         }
 
         if (!previousGamepad2.dpad_up && gamepad2.dpad_up) {
-            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.HIGHJUNCTION));
+//            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.HIGHJUNCTION));
         }
 
         if (!previousGamepad2.dpad_down && gamepad2.dpad_down) {
-            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.GROUNDJUNCTION));
+//            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.GROUNDJUNCTION));
         }
 
         if (!previousGamepad2.dpad_left && gamepad2.dpad_left) {
-            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.MEDIUMJUNCTION));
-
+//            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.MEDIUMJUNCTION));
         }
+
         if (!previousGamepad2.dpad_right && gamepad2.dpad_right) {
-            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.LOWJUNCTION));
+//            liftandServo.generateMotionProfile(liftandServo.distanceToEncoders(IntakeandLiftPID.LOWJUNCTION));
         }
-
-        telemetry.addData("Current Position", liftandServo.currentPosition);
-        telemetry.addData("Current Velocity", liftandServo.currentVelocity);
-        telemetry.addData("Target Position", liftandServo.targetPosition);
-        telemetry.addData("Target Velocity", liftandServo.targetVelocity);
 
         drive.update();
-        liftandServo.followMotionProfile();
+        intake.followMotionProfile();
 
         previousGamepad1.copy(gamepad1);
         previousGamepad2.copy(gamepad2);
