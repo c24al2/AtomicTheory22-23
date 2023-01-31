ca49894e141aabf5a03a4ac464674f3e1dc4816f
maxslarsson <maxslarsson@gmail.com>
2023-01-30 21:43:10 -0500

    Move Intake class into its own package and add an IntakeConstants file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/IntakeConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.intake;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class IntakeConstants {
+    public static double HIGH_JUNCTION_HEIGHT = 2038;
+    public static double MEDIUM_JUNCTION_HEIGHT = 1563;
+    public static double LOW_JUNCTION_HEIGHT = 997;
+    public static double GROUND_JUNCTION_HEIGHT = 255;
+}
