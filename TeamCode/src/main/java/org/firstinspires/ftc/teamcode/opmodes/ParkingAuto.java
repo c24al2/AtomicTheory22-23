9e783d36fa58f1476f650acb76ac1e78bda0e061
Max Larsson <maxslarsson@gmail.com>
2023-02-03 20:16:38 -0500

    Fine tune ParkingAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ParkingAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ParkingAuto.java
@@ -11,13 +11,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.PoseStorage;
 import org.firstinspires.ftc.teamcode.drive.SampleOmniDrive;
+import org.firstinspires.ftc.teamcode.intake.Intake;
+import org.firstinspires.ftc.teamcode.intake.IntakeConstants;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.vision.AprilTagPipeline;
 import org.firstinspires.ftc.teamcode.vision.CameraController;
 
 @Config
 @Autonomous
-public class RightParkingAuto extends LinearOpMode {
+public class ParkingAuto extends LinearOpMode {
     public static Pose2d START_POSE = new Pose2d(36, -62.8, Math.toRadians(90));
 
     @Override
@@ -28,14 +30,22 @@ public class RightParkingAuto extends LinearOpMode {
         AprilTagPipeline aprilTagPipeline = new AprilTagPipeline();
         cameraController.setPipeline(aprilTagPipeline);
 
-        telemetry.addData("Parking Position", aprilTagPipeline.getParkingPosition());
-        telemetry.update();
-
-        cameraController.stopStreaming(); // Reduce resource usage by not processing any more camera
-
         SampleOmniDrive drive = new SampleOmniDrive(hardwareMap);
+        Intake intake = new Intake(hardwareMap);
 
         drive.setPoseEstimate(START_POSE);
+
+        while (!isStarted() && !isStopRequested()) {
+            telemetry.addData("Parking Position", aprilTagPipeline.getParkingPosition());
+            telemetry.update();
+            sleep(25);
+        }
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        intake.closeClaw();
+        intake.followMotionProfileAsync(IntakeConstants.MEDIUM_JUNCTION_HEIGHT);
 
         TrajectorySequence driveToParkingPosition;
 
@@ -43,37 +53,24 @@ public class RightParkingAuto extends LinearOpMode {
             case ZONE1:
                 driveToParkingPosition = drive.trajectorySequenceBuilder(START_POSE)
                         .lineToSplineHeading(new Pose2d(18, -60, Math.toRadians(90)))
-                        .splineToConstantHeading(new Vector2d(12, -36), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(12, -24), Math.toRadians(90))
                         .build();
                 break;
             default: // ZONE2 and NO_TAGS_SEEN
                 driveToParkingPosition = drive.trajectorySequenceBuilder(START_POSE)
-                        .splineTo(new Vector2d(36, -36), Math.toRadians(90))
+                        .splineTo(new Vector2d(36, -24), Math.toRadians(90))
                         .build();
                 break;
             case ZONE3:
                 driveToParkingPosition = drive.trajectorySequenceBuilder(START_POSE)
                         .lineToSplineHeading(new Pose2d(54, -60, Math.toRadians(90)))
-                        .splineToConstantHeading(new Vector2d(60, -36), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(60, -24), Math.toRadians(90))
                         .build();
                 break;
         }
 
-        waitForStart();
-        if (isStopRequested()) return;
+        drive.followTrajectorySequence(driveToParkingPosition);
 
-        drive.followTrajectorySequenceAsync(driveToParkingPosition);
-
-        while (opModeIsActive() && !isStopRequested()) {
-            if (!drive.isBusy()) {
-                break;
-            }
-
-            // We update drive continuously in the background, regardless of state
-            drive.update();
-
-            // Continually write pose to `PoseStorage`
-            PoseStorage.currentPose = drive.getPoseEstimate();
-        }
+        PoseStorage.currentPose = drive.getPoseEstimate();
     }
 }
