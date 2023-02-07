20365c1d6665e9c3bbe1eaecd032160987008621
c24al2 <c24al2@dalton.org>
2023-02-07 07:35:49 -0500

    Odometry Wheel Angle fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/OdometryLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/OdometryLocalizer.java
@@ -15,9 +15,9 @@ import java.util.List;
 @Config
 public class OdometryLocalizer extends ThreeTrackingWheelLocalizer {
     private final static List<Pose2d> WHEEL_POSES = Arrays.asList(
-            new Pose2d(1.7, 0.22, Math.toRadians(270)), //front wheel
-            new Pose2d(-1.161, -1.79, Math.toRadians(30)), // "right" wheel
-            new Pose2d(-1.161, 1.79, Math.toRadians(150))  // "left" wheel
+            new Pose2d(1.7, 0.22, Math.toRadians(90)), //front wheel
+            new Pose2d(-1.161, -1.79, Math.toRadians(210)), // "right" wheel
+            new Pose2d(-1.161, 1.79, Math.toRadians(330))  // "left" wheel
     );
 
     public static double TICKS_PER_REV = 200;
